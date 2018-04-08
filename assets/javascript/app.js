41b973ca0adae5203ebdb4c47893063d53a9b7a3
dhrandy <dhrandy@gmail.com>
2018-04-08 17:48:26 -0400

    added score section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,23 +4,23 @@ function check () {
     var correct = 0;
     var incorrect = 0;
 
-    var htmlCorrect = "Correct!"
-    var htmlQuestion1Answer = "<p>The correct Answer is Manchuria.</p>"
-    var htmlQuestion1Answer = "<p>The correct Answer is Adolf Hitler.</p>"
+    var htmlCorrect = "Correct!";
+    var htmlQuestion1Answer = "<p>The correct Answer is Manchuria.</p>";
+    var htmlQuestion2Answer = "<p>The correct Answer is Adolf Hitler.</p>";
 
         if (question1 == "manchuria"){
             correct++;
-            console.log("correct 1 = "+correct);
+            console.log("correct 1 = " + correct);
             document.querySelector("#question1Answer").innerHTML = htmlCorrect;
         }
         else if (question1 !== "manchuria") {
             incorrect++;
-            console.log("incorrect 1 = "+incorrect);
+            console.log("incorrect 1 = " + incorrect);
             document.querySelector("#question1Answer").innerHTML = htmlQuestion1Answer;
         }
         if (question2 == "adolfHitler"){
             correct++;
-            console.log("correct 2 = "+correct);
+            console.log("correct 2 = " + correct);
             document.querySelector("#question2Answer").innerHTML = htmlCorrect;
         }
         else if (question2 !== "adolfHitler") {
@@ -28,6 +28,10 @@ function check () {
             console.log("incorrect 2 = "+incorrect);
             document.querySelector("#question2Answer").innerHTML = htmlQuestion2Answer;
         }
+
+        var htmlScore = "<p>Correct Answers = " + correct + "<br>" +
+                    "Incorrect Answers = " + incorrect + "</p>";
+        document.querySelector("#score").innerHTML = htmlScore;
 
     console.log("total correct= "+correct);
     console.log("total  incorrect= "+incorrect);

6921a01b3cc6640b7d0a2ec503d80afa1745274e
dhrandy <dhrandy@gmail.com>
2018-04-08 17:34:39 -0400

    updated file

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+function check () {
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var correct = 0;
+    var incorrect = 0;
+
+    var htmlCorrect = "Correct!"
+    var htmlQuestion1Answer = "<p>The correct Answer is Manchuria.</p>"
+    var htmlQuestion1Answer = "<p>The correct Answer is Adolf Hitler.</p>"
+
+        if (question1 == "manchuria"){
+            correct++;
+            console.log("correct 1 = "+correct);
+            document.querySelector("#question1Answer").innerHTML = htmlCorrect;
+        }
+        else if (question1 !== "manchuria") {
+            incorrect++;
+            console.log("incorrect 1 = "+incorrect);
+            document.querySelector("#question1Answer").innerHTML = htmlQuestion1Answer;
+        }
+        if (question2 == "adolfHitler"){
+            correct++;
+            console.log("correct 2 = "+correct);
+            document.querySelector("#question2Answer").innerHTML = htmlCorrect;
+        }
+        else if (question2 !== "adolfHitler") {
+            incorrect++;
+            console.log("incorrect 2 = "+incorrect);
+            document.querySelector("#question2Answer").innerHTML = htmlQuestion2Answer;
+        }
+
+    console.log("total correct= "+correct);
+    console.log("total  incorrect= "+incorrect);
+}
